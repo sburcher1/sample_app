03afccee67bcbdf57536a7eaa3cd275728043146
Scott Burcher <sburcher1@gmail.com>
2014-11-28 21:10:29 +0000

    Added Product scaffolding and basic styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :products
+
 root :to => 'pages#landing_page'
     
     
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20141128112748) do
+
+  create_table "products", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "image_url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
