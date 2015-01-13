d14a0dd3309c8b9d55ba1e614c032ca45c894305
edwardmccaughan <edwardmccaughan@gmail.com>
2015-01-13 20:48:42 +0000

    fixing specs on nitrous.io

--- spec/controllers/product_controller_spec.rb
+++ spec/controllers/product_controller_spec.rb
@@ -1,11 +1,21 @@
 require 'rails_helper' 
 
   describe ProductsController do 
+
+     before do
+       @user = User.create!(email: "a@b.com", password: "password", password_confirmation: "password")
+     end
     
     describe "GET #index" do 
-      it "populates an array of contacts"
-      get :index 
-      assigns(:products).should eq([product])
+      it "populates an array of contacts" do
+        sign_in @user
+        product = Product.create!(name: "woo", description: "woooooooooooooooooooooooooooooooooooooooooooo")
+        get :index 
+        expect(response).not_to be_redirect
+       
+        expect(assigns(:products)).to eq([product])
+        end
+        
     end
 
     describe "GET #show" do 
--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -2,5 +2,27 @@ require 'rails_helper'
 
 describe UsersController do
 
+  before do
+    @user = User.create!(email: "a@b.com", password: "password", password_confirmation: "password")
+  end
+
+  describe "GET show" do
+    context "User is logged in" do
  
+      it "should load the correct user details" do
+        sign_in @user
+        get :show, {id:  @user.id}
+        expect(response.status).to eq 200
+        expect(assigns(:user)).to eq @user
+      end
+    end
+
+    context "No user is logged in" do
+      it "should redirect to login" do
+        get :show,  {id:  @user.id}
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+
 end
