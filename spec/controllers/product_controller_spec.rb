1b4cea796955bd0c6e0da47b41a7a45d3626323c
Scott Burcher <sburcher1@gmail.com>
2015-01-14 07:18:47 +0000

    product controller tests now working

--- spec/controllers/product_controller_spec.rb
+++ spec/controllers/product_controller_spec.rb
@@ -19,14 +19,36 @@ require 'rails_helper'
     end
 
     describe "GET #show" do 
-      it "assigns the requested contact to @contact" 
-      it "renders the :show template" 
+      
+      it "assigns the requested product to @product" do
+        sign_in @user
+        product = Product.create!(name: "woo", description: "woooooooooooooooooooooooooooooooooooooooooooo")
+        expect(assigns(:product)).to eq @product
+      end
+
+      it "renders the :show template" do
+        sign_in @user
+        product = Product.create!(name: "woo", description: "woooooooooooooooooooooooooooooooooooooooooooo")
+        get :show, {id: product.id}
+        expect(response).not_to be_redirect
+      end
+
     end
 
     describe "GET #new" do
-      it "assigns a new Contact to @contact" 
-      it "renders the :new template" 
-    end 
+      it "assigns a new Product to @product" do
+        sign_in @user
+        product = Product.create!(name: "woo", description: "woooooooooooooooooooooooooooooooooooooooooooo")
+        expect(assigns(:product)).to eq @product
+      end
+
+      it "renders the :new template" do
+        sign_in @user
+        product = Product.create!(name: "woo", description: "woooooooooooooooooooooooooooooooooooooooooooo")
+        get :new, {id: product.id}
+        expect(response).not_to be_redirect
+      end
+    end
 
 
   end
